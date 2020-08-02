0c73422951ed769ca183d8b2e393e3b6ba90c836
viniciushrk <viniciushrk@gmail.com>
2020-08-02 13:36:45 -0300

    add components

--- src/App.js
+++ src/App.js
@@ -8,8 +8,21 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#141414" }}>
       <Menu/>
+
+      <BannerMain videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+      url={dadosIniciais.categorias[0].videos[0].url}
+      videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}/>
+
+      <Carousel category={dadosIniciais.categorias[0]}/>
+      <Carousel category={dadosIniciais.categorias[1]}/>
+      <Carousel category={dadosIniciais.categorias[2]}/>
+      <Carousel category={dadosIniciais.categorias[3]}/>
+      <Carousel category={dadosIniciais.categorias[4]}/>
+      <Carousel category={dadosIniciais.categorias[5]}/>
+
+      <Footer />
     </div>
   );
 }
